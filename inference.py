cf1404984ab6542e33af96ef067e166ad78a86b2
manhdung20112000 <manhdung20112000@gmail.com>
2021-12-25 21:48:36 +0700

    update

--- inference.py
+++ inference.py
@@ -1,7 +1,31 @@
+import socketio
+import numpy as np
+
+# connect
+sio = socketio.Client()
+@sio.event
+def connect():
+    print("I'm connected!")
+sio.connect('http://localhost:8000')
+
 
 def inference():
+    # TODO: Left = 0; Right = 1; Other = -1
+    pred = 0
     pass
 
 
+def emit(name='signal', value: int = -1):
+    sio.emit(name, value)
+
 if __name__ == "__main__":
-    inference()
+    # TODO: load data
+
+
+    # predict
+    # pred = inference()
+    pred = np.random.randint(-1, 1, size=100)
+
+    # TODO: send to server
+    for timestamp in pred:
+        emit(value = int(timestamp))
