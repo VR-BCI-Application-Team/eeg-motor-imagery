3814aedcd5f1e7aca9d892ab9dfa83d82e57ab25
manhdung20112000 <manhdung20112000@gmail.com>
2021-12-26 20:38:55 +0700

    inference

--- inference.py
+++ inference.py
@@ -1,5 +1,12 @@
+import time
 import socketio
+import argparse
+
+import torch
 import numpy as np
+
+from model import Classifier
+
 
 # connect
 sio = socketio.Client()
@@ -10,24 +17,44 @@ def connect():
 sio.connect('http://13.212.87.101:8000/')
 
 
-def inference():
-    # TODO: Left = 0; Right = 1; Other = -1
-    pred = 0
-    pass
+def args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_path', type=str, default='')
+    parser.add_argument('--model_path', type=str, default='./model/pretrain_weight.ckpt')
+    parser.add_argument('--lo_thres', type=float, default=0.4)
+    parser.add_argument('--hi_thres', type=float, default=0.6)
+    return parser.parse_args()
+
+
+def inference(model, data, lo_thres=0.5, hi_thres=0.7):
+    # Left = 0; Right = 1; Other = -1
+    model.eval()
+    input = torch.from_numpy(data)[None, None, :].float()
+    pred = model(input).squeeze().detach().numpy()
+
+    if pred > hi_thres:
+        return 1
+    elif pred < lo_thres:
+        return 0
+    else: 
+        return -1
 
 
 def emit(name='signal', value: int = -1):
     sio.emit(name, {'direction': value})
 
-if __name__ == "__main__":
-    # TODO: load data
 
+if __name__ == "__main__":
+    opt = args()
+    # TODO: load input data and model
+    data = np.load(opt.data_path)  # demo
+
+    model = Classifier()
+    model.load_from_checkpoint(opt.model_path)
 
     # predict
-    # pred = inference()
-    pred = np.random.randint(-1, 2, size=100)
+    pred = inference(model, data, opt.lo_thres, opt.hi_thres)
+    print('AFTER_THRES', pred)
 
-    # TODO: send to server
     for timestamp in pred:
-        print(timestamp)
         emit(value = int(timestamp))
